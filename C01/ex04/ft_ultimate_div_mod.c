55aa338f42afd607599607e5ae207ea9d7540c65
larocqueg <portugalgabriel2021@gmail.com>
2024-06-19 16:48:46 +0100

    Fineshed ex03 with success

--- C01/ex04/ft_ultimate_div_mod.c
+++ C01/ex04/ft_ultimate_div_mod.c
@@ -1,18 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: larocqueg <marvin@42.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/19 15:50:50 by larocqueg         #+#    #+#             */
-/*   Updated: 2024/06/19 16:24:33 by larocqueg        ###   ########.fr       */
+/*   Created: 2024/06/19 16:28:41 by larocqueg         #+#    #+#             */
+/*   Updated: 2024/06/19 16:39:34 by larocqueg        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_ft(int *nbr)
+void	ft_ultimate_div_mod(int *a, int *b)
 {
-	*nbr = 42;
+	int	result;
+	int	remainder;
+
+	result = *a / *b;
+	remainder = *a % *b;
+	*a = result;
+	*b = remainder;
 }
