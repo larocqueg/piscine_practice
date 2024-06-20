cd48412798dfa0ca0b3adeb3dffd16de5f580624
larocqueg <portugalgabriel2021@gmail.com>
2024-06-20 16:56:53 +0100

    Fineshed ex07 with success

---
+++ C01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: larocqueg <marvin@42.fr>                   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/20 16:26:25 by larocqueg         #+#    #+#             */
+/*   Updated: 2024/06/20 16:55:08 by larocqueg        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+//#include <stdio.h>
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+	int	temp;
+
+	i = 0;
+	j = size - 1;
+	while (i < j)
+	{
+		temp = tab[i];
+		tab[i] = tab[j];
+		tab[j] = temp;
+		i++;
+		j--;
+	}
+}
+
+/*int	main(void)
+{
+	int counter = 0;
+	int s = 5;
+	int arr[] = {1, 2, 3, 4, 5};
+
+	ft_rev_int_tab(arr, s);
+
+	while(counter <= s - 1)
+		printf("%i ", arr[counter++]);
+	printf("\n");
+	return(0);
+}
+*/
