2acd2c78dd34fa005463796ca9fa4db682fb45f9
larocqueg <portugalgabriel2021@gmail.com>
2024-06-26 17:16:06 +0100

    Fixed fibonacci output to show every step of the function

--- W3Schools practice/Recursion/fibonacci/fibonacci.c
+++ W3Schools practice/Recursion/fibonacci/fibonacci.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
 int	fibonacci(int n)
-{
-	if(n == 0)
-		return(0);
-	if(n == 1)
+{	
+	if(n == 1 || n == 2)
 		return(1);
 	return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 int	main(int argc, char *argv[])
 {
-	int i;
-	printf("Type a number >= 1: ");
-	scanf("%i", &i);
-	printf("The fibonacci of %i is  = %i\n", i, fibonacci(i));
+	int i = 1;
+	int num = 10;
+	printf("Fibonacci:\n");
+	while(i <= num)
+	{
+		printf("%i --> %i\n", i, fibonacci(i));
+		i++;
+	}
 	return(0);
 }
