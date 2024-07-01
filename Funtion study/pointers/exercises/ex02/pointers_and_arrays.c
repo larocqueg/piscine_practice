a9d0e9985023d96e20f2be68151a799a6c5f861b
larocqueg <portugalgabriel2021@gmail.com>
2024-07-01 15:34:21 +0100

    ...

--- Funtion study/pointers/exercises/ex02/pointers_and_arrays.c
+++ Funtion study/pointers/exercises/ex02/pointers_and_arrays.c
@@ -37,7 +37,7 @@ void printArray(int *arr, int size)
 
 int main() 
 {
-	int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	int arr[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 	int size = 10;
 
 	printf("Original array: ");
@@ -51,4 +51,3 @@ int main()
 
     return(0);
 }
-
