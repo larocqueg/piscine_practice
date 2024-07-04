455f53e9cd1a3322e74c020de4f9eaa6589e142b
larocqueg <portugalgabriel2021@gmail.com>
2024-07-04 19:12:56 +0100

    fineshed Level 0

---
+++ 3 - Exams/Exam 00/aff_last_param/aff_last_param.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	if (argc >= 2)
+	{
+		while (argv[argc - 1][i])
+			write (1, &argv[argc - 1][i++], 1);
+	}
+	write(1, "\n", 1);
+	return (0);
+}
