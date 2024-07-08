8967dcd7f0b2d2641754043326e409949cb5075b
larocqueg <portugalgabriel2021@gmail.com>
2024-07-08 16:46:43 +0100

    ..

---
+++ c.lists/C04/ex03/ft_atoi.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: larocqueg <marvin@42.fr>                   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/08 12:46:51 by larocqueg         #+#    #+#             */
+/*   Updated: 2024/07/08 14:13:37 by larocqueg        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	sign;
+	int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
+/*
+int	main(int argc, char *argv[])
+{
+	char	*nbr = argv[1];
+
+	if(argc == 2)
+	{
+		printf("Result is: %i\n", ft_atoi(nbr));
+	}
+	else
+		printf("Missing command line argument!\n");
+	return (0);
+}
+*/
