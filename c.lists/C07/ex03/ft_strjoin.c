3ba0f7de2bf50237cb2701993fb5eee0f4a10be9
larocqueg <portugalgabriel2021@gmail.com>
2024-08-05 21:02:26 +0100

    ex03 C07 100%

--- c.lists/C07/ex03/ft_strjoin.c
+++ c.lists/C07/ex03/ft_strjoin.c
@@ -6,19 +6,78 @@
 /*   By: larocqueg <larocqueg@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 20:35:04 by larocqueg         #+#    #+#             */
-/*   Updated: 2024/07/29 20:39:23 by larocqueg        ###   ########.fr       */
+/*   Updated: 2024/08/05 21:00:26 by larocqueg        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <stdlib.h>
+//#include <stdio.h>
 
-char *ft_strjoin(int size, char **strs, char *sep)
+int	ft_strlen(char *str)
 {
-	
+	int	length;
+
+	length = 0;
+	while (str[length] != '\0')
+		length++;
+	return (length);
 }
 
+char	*ft_strjoin(int size, char **strs, char *sep)
+{
+	int	i;
+	int	j;
+	int	k;
+	int	l;
+	char	*result;
+
+	if (size == 0)
+	{
+		result = (char *)malloc(1);
+		result[0] = '\0';
+		return (result);
+	}
+	i = 0;
+	j = 0;
+	while (i < size)
+	{
+		j += ft_strlen(strs[i]);
+		i++;
+	}
+	j += ft_strlen(sep) * (size - 1);
+	result = (char *)malloc(j + 1);
+	if (!result)
+		return (NULL);
+	i = 0;
+	k = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (strs[i][j])
+		{
+			result[k] = strs[i][j];
+			j++;
+			k++;
+		}
+		if (strs[i][j] == '\0' && i != size - 1)
+		{
+			l = 0;
+			while (sep[l])
+			{
+				result[k] = sep[l];
+				k++;
+				l++;
+			}
+		}
+		i++;
+	}
+	result[k] = '\0';
+	return (result);
+}
+/*
 int	main(void)
 {
-
+	char	*strs[] = {"Hello", "World", "42"};
+	printf("%s\n", ft_strjoin(3, strs, ", "));
 }
+*/
